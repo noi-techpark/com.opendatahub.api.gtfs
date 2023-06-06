afa583d3108d035803d6dc82ef6ea2e73f463670
henri <henri@moosbauer.com>
2023-06-06 16:52:51 +0200

    make reuse compliant

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+SPDX-License-Identifier: CC0-1.0
+-->
+
 # GTFS API server
 This is a simple POC MVP of what could be the Open Data Hub GTFS API
 
--- src/app.mjs
+++ src/app.mjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import * as dotenv from 'dotenv'
 import express, { Router } from 'express'
 import yaml from 'js-yaml'
